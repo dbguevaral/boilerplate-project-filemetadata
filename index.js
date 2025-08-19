dc5a9f8dfbfe77e7fcee783e4a0b3f3edcb069ce
Diego Guevara <brando.guevaral@gmail.com>
2025-08-19 17:46:19 -0500

    Test finished

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const multer = require('multer');
+const upload = multer({storage: multer.memoryStorage()})
 require('dotenv').config()
-
-var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+app.use(express.urlencoded({ extended: true }));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file;
+  res.json({name: file.originalname, type: file.mimetype, size: file.size});
+})
+
